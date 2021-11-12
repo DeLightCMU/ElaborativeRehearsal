0cf2a234b4716ff7a16fdbf4b18c173cc42fbcc4
Shizhe Chen <cszhe1@ruc.edu.cn>
2021-11-12 15:35:29 +0100

    Update download_premodels.sh

--- scripts/download_premodels.sh
+++ scripts/download_premodels.sh
@@ -6,4 +6,5 @@ wget -P datasets/premodels/BiT https://storage.googleapis.com/bit_models/imagene
 wget -P datasets/premodels/BiT https://storage.googleapis.com/bit_models/imagenet21k_wordnet_lemmas.txt
 
 mkdir -p datasets/premodels/TSM
-wget -P datasets/premodels/TSM https://file.lzhu.me/projects/tsm/models/TSM_kinetics_RGB_resnet50_shift8_blockres_avg_segment8_e100_dense.pth
+# wget -P datasets/premodels/TSM https://file.lzhu.me/projects/tsm/models/TSM_kinetics_RGB_resnet50_shift8_blockres_avg_segment8_e100_dense.pth
+wget -P datasets/premodels/TSM https://hanlab.mit.edu/projects/tsm/models/TSM_kinetics_RGB_resnet50_shift8_blockres_avg_segment8_e100_dense.pth
